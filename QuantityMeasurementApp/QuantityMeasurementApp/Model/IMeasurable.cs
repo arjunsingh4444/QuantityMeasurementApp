defab2a17f4428531bea575329a9dd0711d253d2
arjun singh <arjunvkas@gmail.com>
2026-03-08 11:58:34 +0530

    [Arjun] Add. UseCase 10 - GenericConversion

---
+++ QuantityMeasurementApp/QuantityMeasurementApp/Model/IMeasurable.cs
@@ -0,0 +1,8 @@
+namespace QuantityMeasurementApp.Model;
+    public interface IMeasurable
+    {
+        double GetConversionFactor();
+        double ConvertToBaseUnit(double value);
+        double ConvertFromBaseUnit(double baseValue);
+        string GetUnitName();
+    }
